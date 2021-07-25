c6308d9c73bc868a99b014af6098b64146894df9
eugene <shikabli@hubtel.com>
2021-07-25 01:44:43 +0000

    --json

--- server.js
+++ server.js
@@ -30,7 +30,7 @@
       console.log('requested');
       fs.readFile(__dirname + '/package.json', function(err, data) {
         if(err) return next(err);
-        send(JSON.parse(data));
+        res.json(data.toString());
       });
     });
     
